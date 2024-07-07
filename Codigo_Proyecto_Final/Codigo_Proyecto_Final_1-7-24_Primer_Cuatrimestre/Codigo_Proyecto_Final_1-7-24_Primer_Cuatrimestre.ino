f50d76390a71486fc514a9dbb51550f7e0bcac67
EzeOmen527 <ezedodi@gmail.com>
2024-07-07 20:35:50 -0300

    Codigo-de-prueba-Final-2do-bim

--- Codigo_Proyecto_Final/Codigo_Proyecto_Final_1-7-24_Primer_Cuatrimestre/Codigo_Proyecto_Final_1-7-24_Primer_Cuatrimestre.ino
+++ Codigo_Proyecto_Final/Codigo_Proyecto_Final_1-7-24_Primer_Cuatrimestre/Codigo_Proyecto_Final_1-7-24_Primer_Cuatrimestre.ino
@@ -4,23 +4,14 @@
 #include <UniversalTelegramBot.h>
 #include <ESP32Servo.h>
 
-//#define SERVO
-//#define SERVO
-//#define SERVO
-//#define SERVO
-//#define SERVO
-//#define SERVO
-
 Servo ServoMotor[5];
 
-
 bool aux = false;
-
 unsigned long tiempo;
 unsigned long tiempoUltimoCambio;
 
 const char* ssid = "ORT-IoT";
-const char* password = "OrtIOTnew22$2 ";
+const char* password = "OrtIOTnew22$2";
 
 #define BOTtoken "6887192498:AAH96GYOpwVlT3cpzu_IwoZmRzhPGEn1WH4"
 #define CHAT_ID "6861080846"
@@ -28,15 +19,15 @@ const char* password = "OrtIOTnew22$2 ";
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
 
-
 int Botdelay = 1000;
 unsigned long lastTimeBotRan;
 int numNewMessages;
+int estado = 0;
 
 void setup() {
   Serial.begin(9600);
-  for (int i = 0; i <= 5; i++) {
-    ServoMotor[i].attach(12, 13, 14);
+  for (int i = 0; i < 5; i++) {
+    ServoMotor[i].attach(12 + i); 
   }
 
   WiFi.mode(WIFI_STA);
@@ -44,6 +35,7 @@ void setup() {
   client.setCACert(TELEGRAM_CERTIFICATE_ROOT);
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
+    delay(500); 
   }
   Serial.println("");
   Serial.println("WiFi conectado.");
@@ -62,26 +54,25 @@ void loop() {
     lastTimeBotRan = millis();
   }
   tiempo = millis();
+  ServosApagados();
 }
-
 
 void handleNewMessages(int numNewMessages) {
   Serial.println("handleNewMessages");
   Serial.println(String(numNewMessages));
 
   for (int i = 0; i < numNewMessages; i++) {
-
     String chat_id = String(bot.messages[i].chat_id);
     if (chat_id != CHAT_ID) {
       bot.sendMessage(chat_id, "Usuario no autorizado", "");
       continue;
     }
     String text = bot.messages[i].text;
-    int estado = 0;
+
     switch (estado) {
       case 0:
         if (text == "/start") {
-          bot.sendMessage(chat_id, "Programa Braille iniciado ");
+          bot.sendMessage(chat_id, "Programa Braille iniciado");
           estado = 1;
         }
         break;
@@ -89,44 +80,49 @@ void handleNewMessages(int numNewMessages) {
         int text_len = text.length() + 1;
         char char_array_text[text_len];
         text.toCharArray(char_array_text, text_len);
-        const char letra
-        switch (letra) {
-          case "a":
-            servo1();
-            ServosApagados();
-            break;
-          case "b":
-            servo1();
-            servo2();
-            ServosApagados();
+
+        for (int j = 0; j < text_len - 1; j++) { 
+          char letra = char_array_text[j];
+          switch (letra) {
+            case 'a':
+              servo1();
+              break;
+            case 'b':
+              servo1();
+              servo2();
+              break;
+          }
         }
+        estado = 0; 
+        break;
     }
   }
-  //  String from_name = bot.messages[i].from_name;      preguntar a cherniak para que sirve esto
 }
+
 void servo1() {
+  ServoMotor[0].write(180);
+}
+
+void servo2() {
   ServoMotor[1].write(180);
 }
-void servo2() {
+
+void servo3() {
   ServoMotor[2].write(180);
 }
-void servo3() {
+
+void servo4() {
   ServoMotor[3].write(180);
 }
-void servo4() {
+
+void servo5() {
   ServoMotor[4].write(180);
 }
-void servo5() {
-  ServoMotor[5].write(180);
-}
+
 void ServosApagados() {
-  tiempo = millis();
-  if (tiempo >= 1000) {
-    ServoMotor[1].write(0);
-    ServoMotor[2].write(0);
-    ServoMotor[3].write(0);
-    ServoMotor[4].write(0);
-    ServoMotor[5].write(0);
+  if (millis() - tiempo >= 1000) {
+    for (int i = 0; i < 5; i++) {
+      ServoMotor[i].write(0);
+    }
   }
-  //  if (tiempo >= 2000) {}    delay de los servos apagados https://forum.arduino.cc/t/proyecto-para-braille/516633
 }
