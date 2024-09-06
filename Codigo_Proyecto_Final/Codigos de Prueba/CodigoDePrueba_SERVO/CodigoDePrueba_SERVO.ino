3d313560a124cf8e83546270d2ac53e0a7240d3d
Gian <112560707+xHRanger@users.noreply.github.com>
2024-09-06 12:11:33 -0300

    Update CodigoDePrueba_SERVO.ino

--- Codigo_Proyecto_Final/Codigos de Prueba/CodigoDePrueba_SERVO/CodigoDePrueba_SERVO.ino
+++ Codigo_Proyecto_Final/Codigos de Prueba/CodigoDePrueba_SERVO/CodigoDePrueba_SERVO.ino
@@ -14,20 +14,36 @@ void setup() {
  
  //Marcamos en que piines van a estar conectado los servos
   servoMotor1.attach(33);
-  servoMotor2.attach(27);
+  servoMotor2.attach(18);
+  servoMotor3.attach(21);
+
 }
 // pines de los servos 18 21 23 25 26 33  
 void loop ()
 {
 //se escribe nombre del servo.write(el valor del giro);
 servoMotor1.write(90);
+servoMotor2.write(90);
+servoMotor3.write(90);
 delay(1000);
 servoMotor1.write(180);
-delay(1000);
-servoMotor2.write(0);
-delay(1000);
-servoMotor2.write(90);
-delay(1000);
 servoMotor2.write(180);
+servoMotor3.write(180);
+delay(1000);
+servoMotor1.write(0);
+servoMotor2.write(0);
+servoMotor3.write(0);
+delay(1000);
+servoMotor1.write(90);
+servoMotor2.write(90);
+servoMotor3.write(90);
+delay(1000);
+servoMotor1.write(180);
+servoMotor2.write(180);
+servoMotor3.write(180);
+delay(1000);
+servoMotor1.write(0);
+servoMotor2.write(0);
+servoMotor3.write(0);
 delay(1000);
 }
